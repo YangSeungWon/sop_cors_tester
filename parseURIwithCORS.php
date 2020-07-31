ab0e33ea44cba8697e240d0b7b45a254d41ecaeb
YangSeungWon <yysw1109@gmail.com>
2020-07-31 22:30:17 +0900

    to php

---
+++ parseURIwithCORS.php
@@ -0,0 +1,20 @@
+<?php
+    header("Access-Control-Allow-Origin: http://plus15.postech.ac.kr:5757/");
+?>
+
+<div id="scheme">Scheme-></div>
+<div id="host">Host-></div>
+<div id="port">Port-></div>
+<div id="path">Path-></div>
+<div id="query">Query-></div>
+<div id="fragment">Fragment-></div>
+
+<script>
+    const parser = new URL(window.location.href);
+    document.getElementById("scheme").innerText+=parser.protocol+'//';
+    document.getElementById("host").innerText+=parser.hostname;
+    document.getElementById("port").innerText+=':'+parser.port;
+    document.getElementById("path").innerText+=parser.pathname;
+    document.getElementById("query").innerText+=parser.search;
+    document.getElementById("fragment").innerText+=parser.hash;
+</script>
